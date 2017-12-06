499c001f40958a383c7ee6558ad682145fa2efc2
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2017-12-06 14:38:20 +0000

    Create task.py

---
+++ task.py
@@ -0,0 +1,14 @@
+#!/usr/env/python3
+"""
+URL Shortener
+This is the task you need to complete:
+Implement the spec, as described below
+Run the task, through the method offered by the extension
+Follow any additional instructions prompted by the extension
+"""
+
+def shorten_url(input):
+    return input
+
+# testing
+print(shorten_url("https://github.com"))
